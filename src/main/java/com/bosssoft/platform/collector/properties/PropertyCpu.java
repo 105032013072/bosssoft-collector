b87bd8fbdb8757cbe341b60dd152e27cd0b06aa0
huangxuewen <1337893145@qq.com>
2019-11-25 15:51:59 +0800

    初始项目导入，实现收集本地服务器IP，mac，cpu，os信息

---
+++ src/main/java/com/bosssoft/platform/collector/properties/PropertyCpu.java
@@ -0,0 +1,60 @@
+package com.bosssoft.platform.collector.properties;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bosssoft.platform.license.api.Constant;
+
+
+
+public class PropertyCpu implements ServerProperty{
+
+	private static Logger log = LoggerFactory.getLogger(PropertyCpu.class);
+	
+	private String errorMessage;
+	
+	public String propertyName() {
+		
+		return "cpus";
+	}
+
+	public String propertyValue() {
+		//System.out.println("start get cpu info.........");
+		String cpuCnt="";
+		try {
+			String os = System.getProperty("os.name").toLowerCase();
+
+			Process process = null;
+			if (os.indexOf(Constant.LICENSE_OS_NAME_WINDOWS) >= 0)
+				process = Runtime.getRuntime().exec("cmd.exe /c echo %NUMBER_OF_PROCESSORS%");
+			else if (os.indexOf(Constant.LICENSE_OS_NAME_AIX) >= 0)
+				process = Runtime.getRuntime().exec(Constant.IMPRIMATUR_AIXCmd);
+			else if (os.indexOf(Constant.LICENSE_OS_NAME_HP) >= 0)
+				process = Runtime.getRuntime().exec(Constant.IMPRIMATUR_HPUXCmd);
+			else if (os.indexOf(Constant.LICENSE_OS_NAME_LINUX) >= 0)
+				process = Runtime.getRuntime().exec(Constant.IMPRIMATUR_LinuxCmd);
+			else if ((os.indexOf(Constant.LICENSE_OS_NAME_SOLARIS) >= 0) || (os.indexOf(Constant.LICENSE_OS_NAME_SUNOS) >= 0)) {
+				process = Runtime.getRuntime().exec(Constant.IMPRIMATUR_SolarisCmd);
+			}
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			cpuCnt = bufferedReader.readLine().trim();
+			//log.info("get cpu info end,cpu count:"+cpuCnt);
+		} catch (IOException e) {
+			errorMessage="get cpu info fail";
+			log.error("get cpu info fail"+e.toString());
+			
+		} 
+		return cpuCnt;
+	}
+
+	public String getErrorMessage() {
+		
+		return errorMessage;
+	}
+
+}
